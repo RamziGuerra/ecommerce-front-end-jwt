6b3189e4bad4cb551e0bb48bc65ec965fa4f12ab
Ramzi Guerra <gu.ramzy@gmail.com>
2021-11-09 15:48:15 +0100

    auto deploy heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ecommerceFrontEnd'));
+app.use(express.static(__dirname + '/dist/ecommerce-front-end'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/ecommerceFrontEnd/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ecommerce-front-end/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
